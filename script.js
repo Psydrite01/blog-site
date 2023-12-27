c96df5e6dec086f4318badc0dba836404892a9ca
Psydrite <prudompdutta2017@gmail.com>
2023-12-27 21:07:19 +0530

    js script linked

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   const navButton = document.getElementById('dropdown');
-  const navOptions = document.getElementsByClassName('hiddennavbar');
+  const navOptions = document.querySelector('.hiddennavbar');
 
   navButton.addEventListener('click', function () {
-    if (navOptions.style.visibility === 'hidden') {
+    if (navOptions.style.visibility === 'hidden' || navOptions.style.visibility === '') {
       navOptions.style.visibility = 'visible';
     } else {
-      navOptions.style.display = 'hidden';
+      navOptions.style.visibility = 'hidden';
     }
   });
 });
