65df5f866bf9e38dcda825bf7a7dfa7e9c3d16f5
Psydrite <prudompdutta2017@gmail.com>
2023-12-27 19:20:13 +0530

    added dropdown button

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navButton = document.getElementById('dropdown');
+    const navOptions = document.querySelector('.hiddennavbar');
+  
+    navButton.addEventListener('click', function () {
+      if (navOptions.style.visibility === 'hidden' || navOptions.style.visibility === '') {
+        navOptions.style.visibility = 'visible';
+      } else {
+        navOptions.style.visibility = 'hidden';
+      }
+    });
+  });
