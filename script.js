5817dfe76cb1626ca4049264c8057bc5e949e575
Psydrite <prudompdutta2017@gmail.com>
2023-12-27 19:50:57 +0530

    fixed some more issues

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navButton = document.getElementById('dropdown');
-    const navOptions = document.querySelector('.hiddennavbar');
-  
-    navButton.addEventListener('click', function () {
-      if (navOptions.style.visibility === 'hidden' || navOptions.style.visibility === '') {
-        navOptions.style.visibility = 'visible';
-      } else {
-        navOptions.style.visibility = 'hidden';
-      }
-    });
+  const navButton = document.getElementById('dropdown');
+  const navOptions = document.querySelector('.hiddennavbar');
+
+  navButton.addEventListener('click', function () {
+    if (navOptions.style.display === 'none' || navOptions.style.display === '') {
+      navOptions.style.display = 'block';
+    } else {
+      navOptions.style.display = 'none';
+    }
   });
+});
